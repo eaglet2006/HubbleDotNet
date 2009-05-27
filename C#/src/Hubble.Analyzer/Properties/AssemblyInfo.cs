afe903c325cad6b223fca822ed16d71790894968
eaglet2006_cp <SND\eaglet2006_cp>
2009-05-27 01:04:44 +0000

    Version 0.2 (Final version)

--- C#/src/Hubble.Analyzer/Properties/AssemblyInfo.cs
+++ C#/src/Hubble.Analyzer/Properties/AssemblyInfo.cs
@@ -1,4 +1,21 @@
-﻿using System.Reflection;
+﻿/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ * 
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
@@ -8,7 +25,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTitle("Hubble.Analyzer")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("VenueMedia")]
+[assembly: AssemblyCompany("Hubble.Net")]
 [assembly: AssemblyProduct("Hubble.Analyzer")]
 [assembly: AssemblyCopyright("Copyright © VenueMedia 2009")]
 [assembly: AssemblyTrademark("")]
@@ -32,5 +49,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("0.0.2.0")]
+[assembly: AssemblyFileVersion("0.0.2.0")]
