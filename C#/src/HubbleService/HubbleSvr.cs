a3dcee3a38a199c8991d6a876d39f52433ea86e1
eaglet2006_cp <SND\eaglet2006_cp>
2012-04-12 11:49:34 +0000

    V1.2.2.0 open source hubbleservice

---
+++ C#/src/HubbleService/HubbleSvr.cs
@@ -0,0 +1,159 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.ServiceProcess;
+using System.Text;
+
+namespace HubbleService
+{
+    public partial class HubbleSvr : ServiceBase
+    {
+        public HubbleSvr()
+        {
+            InitializeComponent();
+        }
+
+        protected override void OnStart(string[] args)
+        {
+            Process hubbleTask = new Process();
+
+            string defaultDir = TaskInformation.GetInstanceDir("Default");
+
+            hubbleTask.StartInfo.FileName = defaultDir + "HubbleTask.exe";
+            hubbleTask.StartInfo.Arguments = "\"" + defaultDir + "\"";
+            hubbleTask.StartInfo.UseShellExecute = false;
+            hubbleTask.StartInfo.RedirectStandardInput = false;
+            hubbleTask.StartInfo.RedirectStandardOutput = false;
+            hubbleTask.StartInfo.RedirectStandardError = false;
+            hubbleTask.StartInfo.CreateNoWindow = true;
+            hubbleTask.StartInfo.WorkingDirectory = defaultDir;
+
+            hubbleTask.Start();
+        }
+
+        protected override void OnStop()
+        {
+            //Process defaultProcess = null;
+
+            string defaultDir = TaskInformation.GetInstanceDir("Default");
+
+
+            {
+                Process[] processes = Process.GetProcessesByName("HubbleTask");
+
+                string dir = defaultDir;
+
+                if (processes.Length <= 0)
+                {
+                    return;
+                }
+
+                //foreach (Process p in processes)
+                //{
+                //    string targetDir = System.IO.Path.GetDirectoryName(p.MainModule.FileName);
+
+                //    targetDir = Hubble.Framework.IO.Path.AppendDivision(targetDir, '\\');
+
+                //    if (targetDir.Equals(dir, StringComparison.CurrentCultureIgnoreCase))
+                //    {
+                //        defaultProcess = p;
+                //        break;
+                //    }
+                //}
+
+                //if (defaultProcess == null)
+                //{
+                //    return;
+                //}
+            }
+
+            try
+            {
+
+                Hubble.Framework.Net.TcpClient tcpClient = new Hubble.Framework.Net.TcpClient();
+                tcpClient.Port = TaskInformation.GetTcpPort("Default");
+                tcpClient.SendSyncMessage(1001, TaskInformation.GetStartTime("Default"));
+                tcpClient.Close();
+            }
+            catch(Exception e)
+            {
+                System.Threading.Thread.Sleep(5000);
+
+                try
+                {
+                    Hubble.Framework.IO.File.WriteLine(defaultDir + "hubblesvr.log",
+                        string.Format("Close hubble.net fail! err:{0}", e.Message));
+                }
+                catch
+                {
+                }
+
+                Process[] processes = Process.GetProcessesByName("HubbleTask");
+
+                foreach (Process p in processes)
+                {
+                    try
+                    {
+                        p.Kill();
+                    }
+                    catch(Exception e1)
+                    {
+                        Hubble.Framework.IO.File.WriteLine(defaultDir + "hubblesvr.log",
+                            string.Format("Kill fail hubble.net fail! err:{0}", e1.Message));
+                    }
+                }
+            }
+
+            int timeout = 600;
+            while (timeout-- > 0)
+            {
+                System.Threading.Thread.Sleep(1000);
+
+                Process[] processes = Process.GetProcessesByName("HubbleTask");
+
+                string dir = defaultDir;
+                bool stillRunning = false;
+
+                if (processes.Length <= 0)
+                {
+                    return;
+                }
+
+                //foreach (Process p in processes)
+                //{
+                //    string targetDir = System.IO.Path.GetDirectoryName(p.MainModule.FileName);
+
+                //    targetDir = Hubble.Framework.IO.Path.AppendDivision(targetDir, '\\');
+
+                //    if (targetDir.Equals(dir, StringComparison.CurrentCultureIgnoreCase))
+                //    {
+                //        stillRunning = true;
+                //        break;
+                //    }
+                //}
+
+                //if (!stillRunning)
+                //{
+                //    return;
+                //}
+
+                try
+                {
+                    if ((timeout % 10) == 0)
+                    {
+                        Hubble.Framework.IO.File.WriteLine(defaultDir + "hubblesvr.log",
+                            string.Format("Watting hubble task exit. Process count:{0}", processes.Length));
+                    }
+                }
+                catch
+                {
+                }
+
+            }
+
+            throw new Exception("Stop hubble.net service timeout!");
+        }
+    }
+}
---
+++ C#/src/HubbleService/Properties/AssemblyInfo.cs
@@ -0,0 +1,41 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("HubbleService")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Hubble Star Pty")]
+#if HubblePro
+[assembly: AssemblyProduct("HubblePro 2012 Beta 1 Edition")]
+[assembly: AssemblyCopyright("Copyright © Hubble Star Pty 2011")]
+#else
+[assembly: AssemblyProduct("HubbleDotNet Community Edition")]
+[assembly: AssemblyCopyright("Copyright © eaglet 2009-2011")]
+#endif
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("534f9a30-ef1a-4970-a0ea-9864b0fd78f9")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.2.2.0")]
+[assembly: AssemblyFileVersion("1.2.2.0")]
